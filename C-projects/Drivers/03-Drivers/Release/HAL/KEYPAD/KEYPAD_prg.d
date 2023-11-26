4a0d95f8286a6e6383ba7d16f2608455f43adc8e
Raheek_Helal <helalraheek@gmail.com>
2023-11-26 04:08:41 +0200

    reorganize

--- C-projects/Drivers/03-Drivers/Release/HAL/KEYPAD/KEYPAD_prg.d
+++
@@ -1,9 +0,0 @@
-HAL/KEYPAD/KEYPAD_prg.o HAL/KEYPAD/KEYPAD_prg.o:  \
- ../HAL/KEYPAD/KEYPAD_prg.c ../HAL/KEYPAD/../../LIB/STD_TYPES.h \
-  ../HAL/KEYPAD/KEYPAD_int.h ../HAL/KEYPAD/../../MCAL/DIO/DIO_int.h
-
-../HAL/KEYPAD/../../LIB/STD_TYPES.h:
-
-../HAL/KEYPAD/KEYPAD_int.h:
-
-../HAL/KEYPAD/../../MCAL/DIO/DIO_int.h:
--- C-projects/Drivers/03-Drivers/Release/MCAL/DIO/DIO_prg.d
+++
@@ -1,9 +0,0 @@
-MCAL/DIO/DIO_prg.o MCAL/DIO/DIO_prg.o: ../MCAL/DIO/DIO_prg.c \
-  ../MCAL/DIO/../../LIB/STD_TYPES.h ../MCAL/DIO/../../LIB/BIT_MATH.h \
-  ../MCAL/DIO/DIO_int.h
-
-../MCAL/DIO/../../LIB/STD_TYPES.h:
-
-../MCAL/DIO/../../LIB/BIT_MATH.h:
-
-../MCAL/DIO/DIO_int.h:
